45c3daffabbb8f86adc6d088bd3e087e51fe5250
Bricktheworld <Bricktheworld@users.noreply.github.com>
2017-05-04 11:01:44 -0700

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ var lazerrs = [];
 var lazerls = [];
 var ties = [];
 var tieimg;
-var explosions = [];
+//var explosions = [];
 function preload() {
   tieimg = loadImage("tie.png");
 }
@@ -201,7 +201,7 @@ function draw(){
     //tie fighters
     for(i = ties.length-1; i >= 0; i--){
         if(ties[i].xp <= 0){
-             //explosions.push(new Explosion(ties[i].x + ties[i].pos.x,ties[i].y + ties[i].pos.y);
+              //explosions.push(new Explosion(ties[i].x + ties[i].pos.x,ties[i].y + ties[i].pos.y);
               //explosions[i].update();
               //explosions[i].show();
         }
