0be60d4e4de35cce07c74e11ba32e8deb894b734
Bricktheworld <Bricktheworld@users.noreply.github.com>
2017-05-06 20:48:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -263,9 +263,9 @@ function draw(){
     rotate(45);
     rect(0,0,10,height);
     pop();
-    push();
-    translate(width/2,height/2);
-    rotate(45);
+    //push();
+    //translate(width/2,height/2);
+    //rotate(45);
     rect(width/2,height/2,10,height);
-    pop();
+    //pop();
 }
