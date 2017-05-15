3ba68fde23e635d170bd19437337b86a7083ed2b
Bricktheworld <Bricktheworld@users.noreply.github.com>
2017-05-15 10:29:47 -0700

    Update code.js

--- code.js
+++ code.js
@@ -258,6 +258,6 @@ function draw(){
     }
     stroke(132, 132, 132);
     strokeWeight(20);
-    line(0,0,width/2-20, height/2);
-    line(width,0,width/2 + 20,height/2);
+    line(0,0,width/2 - 20, height/2+40);
+    line(width,0,width/2 + 20,height/2+40);
 }
